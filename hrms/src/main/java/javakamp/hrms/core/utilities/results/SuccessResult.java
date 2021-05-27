eb871f5d50a1c1b845a1e3d12b48c7cb93ae6ebf
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-28 00:08:35 +0300

    Create SuccessResult.java

---
+++ hrms/src/main/java/javakamp/hrms/core/utilities/results/SuccessResult.java
@@ -0,0 +1,13 @@
+package javakamp.hrms.core.utilities.results;
+
+public class SuccessResult extends Result {
+
+	public SuccessResult(boolean success, String message) {
+		super(true, message);
+	}
+	
+	public SuccessResult(boolean success) {
+		super(true);
+	}
+
+}
