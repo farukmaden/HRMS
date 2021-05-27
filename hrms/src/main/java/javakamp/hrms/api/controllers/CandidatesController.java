7ed51c9b3474468cf7706d1900b753ac491dda2f
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-28 00:09:21 +0300

    Create CandidatesController.java

---
+++ hrms/src/main/java/javakamp/hrms/api/controllers/CandidatesController.java
@@ -0,0 +1,37 @@
+package javakamp.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javakamp.hrms.business.abstracts.CandidatesService;
+import javakamp.hrms.core.utilities.results.DataResult;
+import javakamp.hrms.core.utilities.results.Result;
+import javakamp.hrms.entities.concretes.Candidates;
+
+@RestController
+@RequestMapping("/api/candidates")
+public class CandidatesController {
+
+	private CandidatesService candidatesService;
+
+	@Autowired
+	public CandidatesController(CandidatesService candidatesService) {
+		super();
+		this.candidatesService = candidatesService;
+	}
+	@GetMapping("/getall")
+	public DataResult<List<Candidates>> getAll(){
+		return this.candidatesService.getAll();
+	}
+	@PostMapping("/record")
+	public Result record(@RequestBody Candidates candidates) {
+		return this.candidatesService.record(candidates);
+	}
+	
+}
